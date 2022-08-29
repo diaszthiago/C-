11848f3c636ffe96ad1c461525dc7592c09690ff
diaszthiago <91074951+diaszthiago@users.noreply.github.com>
2022-08-29 17:07:54 -0300

    Add files via upload

---
+++ Uni Locadora/FrmCadastroCliente.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Uni_Locadora
+{
+    partial class FrmCadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCadastroCliente));
+            this.lbCodigo = new System.Windows.Forms.Label();
+            this.lbCPF = new System.Windows.Forms.Label();
+            this.lbDtNasc = new System.Windows.Forms.Label();
+            this.lbNome = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.txtDtnasc = new System.Windows.Forms.MaskedTextBox();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.btPesquisar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbCodigo
+            // 
+            this.lbCodigo.AutoSize = true;
+            this.lbCodigo.Location = new System.Drawing.Point(12, 54);
+            this.lbCodigo.Name = "lbCodigo";
+            this.lbCodigo.Size = new System.Drawing.Size(60, 18);
+            this.lbCodigo.TabIndex = 0;
+            this.lbCodigo.Text = "Código:";
+            // 
+            // lbCPF
+            // 
+            this.lbCPF.AutoSize = true;
+            this.lbCPF.Location = new System.Drawing.Point(209, 54);
+            this.lbCPF.Name = "lbCPF";
+            this.lbCPF.Size = new System.Drawing.Size(42, 18);
+            this.lbCPF.TabIndex = 1;
+            this.lbCPF.Text = "CPF:";
+            // 
+            // lbDtNasc
+            // 
+            this.lbDtNasc.AutoSize = true;
+            this.lbDtNasc.Location = new System.Drawing.Point(393, 54);
+            this.lbDtNasc.Name = "lbDtNasc";
+            this.lbDtNasc.Size = new System.Drawing.Size(147, 18);
+            this.lbDtNasc.TabIndex = 2;
+            this.lbDtNasc.Text = "Data de Nascimento:";
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Location = new System.Drawing.Point(12, 174);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(53, 18);
+            this.lbNome.TabIndex = 3;
+            this.lbNome.Text = "Nome:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(15, 75);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 24);
+            this.txtCodigo.TabIndex = 4;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 195);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(300, 24);
+            this.txtNome.TabIndex = 5;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(212, 75);
+            this.txtCPF.Mask = "000,000,000-00";
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 24);
+            this.txtCPF.TabIndex = 6;
+            this.txtCPF.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // txtDtnasc
+            // 
+            this.txtDtnasc.Location = new System.Drawing.Point(396, 75);
+            this.txtDtnasc.Mask = "00/00/0000";
+            this.txtDtnasc.Name = "txtDtnasc";
+            this.txtDtnasc.Size = new System.Drawing.Size(100, 24);
+            this.txtDtnasc.TabIndex = 7;
+            this.txtDtnasc.ValidatingType = typeof(System.DateTime);
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.Enabled = false;
+            this.btExcluir.Location = new System.Drawing.Point(142, 251);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(109, 33);
+            this.btExcluir.TabIndex = 8;
+            this.btExcluir.Text = "Excluir Cliente";
+            this.btExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(15, 252);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(114, 32);
+            this.btSalvar.TabIndex = 9;
+            this.btSalvar.Text = "Incluir Cliente";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // btPesquisar
+            // 
+            this.btPesquisar.Location = new System.Drawing.Point(15, 316);
+            this.btPesquisar.Name = "btPesquisar";
+            this.btPesquisar.Size = new System.Drawing.Size(125, 33);
+            this.btPesquisar.TabIndex = 10;
+            this.btPesquisar.Text = "Procurar Cliente";
+            this.btPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // FrmCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 385);
+            this.Controls.Add(this.btPesquisar);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.btExcluir);
+            this.Controls.Add(this.txtDtnasc);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lbNome);
+            this.Controls.Add(this.lbDtNasc);
+            this.Controls.Add(this.lbCPF);
+            this.Controls.Add(this.lbCodigo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmCadastroCliente";
+            this.Text = "Cadastro de Clientes";
+            this.Load += new System.EventHandler(this.FrmCadastroCliente_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbCodigo;
+        private System.Windows.Forms.Label lbCPF;
+        private System.Windows.Forms.Label lbDtNasc;
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.MaskedTextBox txtDtnasc;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Button btPesquisar;
+    }
+}
