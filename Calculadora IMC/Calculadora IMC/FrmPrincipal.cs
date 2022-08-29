449ccd7f6f7a11f470511391e7b611781b629fc0
diaszthiago <91074951+diaszthiago@users.noreply.github.com>
2022-08-29 17:01:28 -0300

    Add files via upload

---
+++ Calculadora IMC/Calculadora IMC/FrmPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculadora_IMC
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            Imc imc = new Imc();
+            imc.Altura = float.Parse(txtAltura.Text);
+            imc.Peso = float.Parse(txtPeso.Text);
+            float resultadoIMC = imc.calculaImc();
+
+            lbResultadoIMC.Text = "IMC: " + resultadoIMC.ToString("0.00");
+            lbResultadoIMC.Visible = true;
+
+            lbClassificacao.Text = "Classificação: " + imc.classificaIMC(resultadoIMC);
+            lbClassificacao.Visible = true;
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Calculadora IMC/Calculadora IMC/Imc.cs
@@ -0,0 +1,66 @@
+﻿namespace Calculadora_IMC
+{
+    public class Imc
+    {
+        private float peso;
+        private float altura;
+
+        public float Altura
+        {
+            get => altura;
+            set => altura = value;
+        }
+
+        public float Peso
+        {
+            get => peso;
+            set => peso = value;
+        }
+
+        public float calculaImc()
+        {
+            float imc = peso / (altura * altura);
+            return imc;
+        }
+
+        public string classificaIMC(float imc)
+        {
+            string classificacao = "";
+
+            if (imc < 16)
+            {
+                classificacao = "Magreza grau III";
+            }
+            else if (imc >= 16 && imc <= 16.9)
+            {
+                classificacao = "Magreza grau II";
+            }
+            else if (imc >= 17 && imc <= 18.4)
+            {
+                classificacao = "Magreza grau I";
+            }
+            else if (imc >= 18.5 && imc < 25)
+            {
+                classificacao = "Adequado";
+            }
+            else if (imc >= 25 && imc < 30)
+            {
+                classificacao = "Pré-obeso";
+            }
+            else if (imc >= 30 && imc < 35)
+            {
+                classificacao = "Obesidade grau I";
+            }
+            else if (imc >= 36 && imc <= 40)
+            {
+                classificacao = "Obesidade grau II";
+            }
+            else
+            {
+                classificacao = "Obseidade grau III";
+            }
+
+            return classificacao;
+        }
+    }
+}
