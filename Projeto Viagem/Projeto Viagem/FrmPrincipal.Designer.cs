ee29350cd71a66dcd2a6b48f9cf37de7b669700a
diaszthiago <91074951+diaszthiago@users.noreply.github.com>
2022-08-29 17:05:29 -0300

    Add files via upload

---
+++ Projeto Viagem/Projeto Viagem/FrmPrincipal.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace Projeto_Viagem
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.lbDistancia = new System.Windows.Forms.Label();
+            this.lbConsumo = new System.Windows.Forms.Label();
+            this.lbCombustivel = new System.Windows.Forms.Label();
+            this.lbPedagio = new System.Windows.Forms.Label();
+            this.ckGastoPedagio = new System.Windows.Forms.CheckBox();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.txtConsumo = new System.Windows.Forms.TextBox();
+            this.txtCombustivel = new System.Windows.Forms.TextBox();
+            this.txtPedagio = new System.Windows.Forms.TextBox();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbDistancia
+            // 
+            this.lbDistancia.AutoSize = true;
+            this.lbDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDistancia.Location = new System.Drawing.Point(12, 19);
+            this.lbDistancia.Name = "lbDistancia";
+            this.lbDistancia.Size = new System.Drawing.Size(160, 20);
+            this.lbDistancia.TabIndex = 0;
+            this.lbDistancia.Text = "Distância a percorrer:";
+            // 
+            // lbConsumo
+            // 
+            this.lbConsumo.AutoSize = true;
+            this.lbConsumo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbConsumo.Location = new System.Drawing.Point(12, 59);
+            this.lbConsumo.Name = "lbConsumo";
+            this.lbConsumo.Size = new System.Drawing.Size(202, 20);
+            this.lbConsumo.TabIndex = 1;
+            this.lbConsumo.Text = "Consumo médio do veículo:";
+            // 
+            // lbCombustivel
+            // 
+            this.lbCombustivel.AutoSize = true;
+            this.lbCombustivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCombustivel.Location = new System.Drawing.Point(12, 98);
+            this.lbCombustivel.Name = "lbCombustivel";
+            this.lbCombustivel.Size = new System.Drawing.Size(184, 20);
+            this.lbCombustivel.TabIndex = 2;
+            this.lbCombustivel.Text = "Valor do combustível: R$";
+            // 
+            // lbPedagio
+            // 
+            this.lbPedagio.AutoSize = true;
+            this.lbPedagio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPedagio.Location = new System.Drawing.Point(12, 182);
+            this.lbPedagio.Name = "lbPedagio";
+            this.lbPedagio.Size = new System.Drawing.Size(222, 20);
+            this.lbPedagio.TabIndex = 3;
+            this.lbPedagio.Text = "Valor gasto com pedágios: R$";
+            this.lbPedagio.Visible = false;
+            // 
+            // ckGastoPedagio
+            // 
+            this.ckGastoPedagio.AutoSize = true;
+            this.ckGastoPedagio.BackColor = System.Drawing.Color.Transparent;
+            this.ckGastoPedagio.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ckGastoPedagio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckGastoPedagio.Location = new System.Drawing.Point(12, 135);
+            this.ckGastoPedagio.Name = "ckGastoPedagio";
+            this.ckGastoPedagio.Size = new System.Drawing.Size(213, 24);
+            this.ckGastoPedagio.TabIndex = 4;
+            this.ckGastoPedagio.Text = "Houve gasto com pedágio";
+            this.ckGastoPedagio.UseVisualStyleBackColor = false;
+            this.ckGastoPedagio.CheckedChanged += new System.EventHandler(this.ckGastoPedagio_CheckedChanged);
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(240, 19);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(63, 20);
+            this.txtDistancia.TabIndex = 5;
+            // 
+            // txtConsumo
+            // 
+            this.txtConsumo.Location = new System.Drawing.Point(240, 59);
+            this.txtConsumo.Name = "txtConsumo";
+            this.txtConsumo.Size = new System.Drawing.Size(63, 20);
+            this.txtConsumo.TabIndex = 6;
+            // 
+            // txtCombustivel
+            // 
+            this.txtCombustivel.Location = new System.Drawing.Point(240, 98);
+            this.txtCombustivel.Name = "txtCombustivel";
+            this.txtCombustivel.Size = new System.Drawing.Size(63, 20);
+            this.txtCombustivel.TabIndex = 7;
+            // 
+            // txtPedagio
+            // 
+            this.txtPedagio.Location = new System.Drawing.Point(240, 182);
+            this.txtPedagio.Name = "txtPedagio";
+            this.txtPedagio.Size = new System.Drawing.Size(63, 20);
+            this.txtPedagio.TabIndex = 8;
+            this.txtPedagio.Visible = false;
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.BackColor = System.Drawing.Color.SpringGreen;
+            this.btCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalcular.Location = new System.Drawing.Point(12, 221);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(291, 42);
+            this.btCalcular.TabIndex = 9;
+            this.btCalcular.Text = "Calcular gastos";
+            this.btCalcular.UseVisualStyleBackColor = false;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(309, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Km";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(309, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Km/L";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(394, 322);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.txtPedagio);
+            this.Controls.Add(this.txtCombustivel);
+            this.Controls.Add(this.txtConsumo);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.ckGastoPedagio);
+            this.Controls.Add(this.lbPedagio);
+            this.Controls.Add(this.lbCombustivel);
+            this.Controls.Add(this.lbConsumo);
+            this.Controls.Add(this.lbDistancia);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.Text = "Gasto da Viagem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbDistancia;
+        private System.Windows.Forms.Label lbConsumo;
+        private System.Windows.Forms.Label lbCombustivel;
+        private System.Windows.Forms.Label lbPedagio;
+        private System.Windows.Forms.CheckBox ckGastoPedagio;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.TextBox txtConsumo;
+        private System.Windows.Forms.TextBox txtCombustivel;
+        private System.Windows.Forms.TextBox txtPedagio;
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Projeto Viagem/Projeto Viagem/FrmRelatorio.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Projeto_Viagem
+{
+    partial class FrmRelatorio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtRelatorio = new System.Windows.Forms.RichTextBox();
+            this.btFechar = new System.Windows.Forms.Button();
+            this.btExportar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtRelatorio
+            // 
+            this.rtRelatorio.Location = new System.Drawing.Point(12, 12);
+            this.rtRelatorio.Name = "rtRelatorio";
+            this.rtRelatorio.Size = new System.Drawing.Size(424, 209);
+            this.rtRelatorio.TabIndex = 0;
+            this.rtRelatorio.Text = "";
+            // 
+            // btFechar
+            // 
+            this.btFechar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btFechar.Location = new System.Drawing.Point(346, 227);
+            this.btFechar.Name = "btFechar";
+            this.btFechar.Size = new System.Drawing.Size(90, 27);
+            this.btFechar.TabIndex = 2;
+            this.btFechar.Text = "Fechar";
+            this.btFechar.UseVisualStyleBackColor = false;
+            this.btFechar.Click += new System.EventHandler(this.btFechar_Click);
+            // 
+            // btExportar
+            // 
+            this.btExportar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btExportar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExportar.Location = new System.Drawing.Point(182, 227);
+            this.btExportar.Name = "btExportar";
+            this.btExportar.Size = new System.Drawing.Size(148, 27);
+            this.btExportar.TabIndex = 3;
+            this.btExportar.Text = "Exportar para TXT";
+            this.btExportar.UseVisualStyleBackColor = false;
+            this.btExportar.Click += new System.EventHandler(this.btExportar_Click);
+            // 
+            // FrmRelatorio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 317);
+            this.Controls.Add(this.btExportar);
+            this.Controls.Add(this.btFechar);
+            this.Controls.Add(this.rtRelatorio);
+            this.Name = "FrmRelatorio";
+            this.Text = "Relatório da Viagem";
+            this.Load += new System.EventHandler(this.FrmRelatorio_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtRelatorio;
+        private System.Windows.Forms.Button btFechar;
+        private System.Windows.Forms.Button btExportar;
+    }
+}
